9e47406513d937d58a6ef24ab67d818958470211
Crystal Craig <ckcraig@amarillocollege.com>
2025-03-18 14:01:19 -0500

    make some changes

--- Main.java
+++ Main.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;  // Import the Scanner class
+
 public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello world");
+        //System.out.println("Hello world");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
+
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);  // Output user input
+
+        //prompt user input year of birth
+
+        System.out.println("what is your year of birth?");
+        
+        int year = myObj.nextInt();
+
+        int age = 2025 -year;
+        System.out.println("you are " + age  + " years old");
+
+        String decimal = "3.45";
+        String integer = "4567";
+        double double1 = Double.parseDouble(decimal); //convert to double
+
+        int int1 = Integer.parseInt(decimal); //convert to int
+
+        System.out.println("double number" +double1);
+        System.out.println("int number " +int1);
+
+
     }
 }
